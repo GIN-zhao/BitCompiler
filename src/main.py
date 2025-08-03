3c6c666b83ba418b6f337b4253989517859b2f40
root <1599601858@qq.com>
2025-08-03 08:59:05 +0000

    init

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import copy
+import logging
+
+from runtime import Runtime
+from common import Params, load_configs, creat_unique_experiment_name
+import fire
+import os 
+
+os.environ['https_proxy']= 'http://10.20.207.237:7890'
+os.environ['http_proxy']= 'http://10.20.207.237:7890'
+class EntryPoint(object):
+    experiment = None
+    configs = None
+
+    def __init__(self, configs: str = "../configs/example.jsonnet"):
+
+        _config = load_configs(configs)
+        exp_name = creat_unique_experiment_name(_config)
+        _config_copy = copy.deepcopy(_config)
+        self.experiment = Runtime.from_params(Params(_config))
+        self.experiment.setup(exp_name, _config_copy)
+
+    def __getattr__(self, attr):
+        if attr in self.__class__.__dict__:
+            return getattr(self, attr)
+        else:
+            return getattr(self.experiment, attr)
+
+    def __dir__(self):
+        return sorted(set(super().__dir__() + self.experiment.__dir__()))
+
+
+if __name__ == '__main__':
+    fire.Fire(EntryPoint)
